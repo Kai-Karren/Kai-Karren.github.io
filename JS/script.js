f394deb090c2c261cbd120ab9aacd0b8aeac8412
KKaiKK <KKaiKK@web.de>
2014-10-28 13:38:04 +0100

    I added a few simple JavaScript functions

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,28 @@
 
 console.log("Hallo");
 
+
+function addNumbers(a, b) {
+    
+    return a + b;
+    
+}
+
+function substrateNumbers(a, b) {
+    
+    return a - b;
+}
+
+function multiplicateNumbers(a, b) {
+
+    return a * b;
+}
+
+function divideNumbers(a, b) {
+
+    return a / b;
+}
+
+
+
+
